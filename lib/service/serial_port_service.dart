83a2d25dd7b8a0397118a6703ceb28e747abffb6
Daniel Hammershoi <dbh@littlegiants.dk>
2025-07-08 16:53:26 +0200

    added end bit

--- lib/service/serial_port_service.dart
+++ lib/service/serial_port_service.dart
@@ -11,79 +11,86 @@ class RelayService {
   }
 
   Future<void> sendONCommand20(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x20;
     bytes[2] = relay;
     bytes[3] = 0x00;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     await port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendOFFCommand21(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x21;
     bytes[2] = relay;
     bytes[3] = 0x00;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendTimerCommand22(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x22;
     bytes[2] = relay;
     bytes[3] = 0x05;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendONCommand11(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x11;
     bytes[2] = relay;
     bytes[3] = 0x00;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendOFFCommand12(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x12;
     bytes[2] = relay;
     bytes[3] = 0x00;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendTimerCommand41(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x41;
     bytes[2] = relay;
     bytes[3] = 0x05;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
   Future<void> sendToggleRelayCommand(int relay) async {
-    final bytes = Uint8List(6);
+    final bytes = Uint8List(7);
     bytes[0] = 0x04;
     bytes[1] = 0x14;
     bytes[2] = relay;
     bytes[3] = 0x00;
     bytes[4] = 0x00;
     bytes[5] = calculateChecksum(bytes);
+    bytes[6] = 0x0f;
     port.writeBytesFromUint8List(bytes);
   }
 
